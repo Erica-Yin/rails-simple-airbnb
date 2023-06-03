199604026817fafc4b3b46bf1457ea38ac7950d8
Erica Yin <ericayin0225@gmail.com>
2023-06-03 16:33:26 +0800

    can be better

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+flat_1 = Flat.create(
+  name: 'Tree House amidst forest @ Kailasa Woods',
+  address: 'Dehradun, Uttarakhand, India',
+  description: 'Entire tree house is yours, you can come and share our Den, where you can watch movies, play board games, play guitar.
+  You can use our informal dining area and have breakfast, lunch etc having a personal forest view...',
+  price_per_night: 1719,
+  number_of_guests: 3,
+  picture_url: 'https://z1.muscache.cn/pictures/c79965b5-9b22-4504-af7b-3131f5c25dfa.jpg?im_w=1200'
+)
+
+flat_2 = Flat.create(
+  name: 'Bamboo Turtle Ecolodge river view 5km Ubud center',
+  address: 'Kecamatan Ubud, Bali, Indonesia',
+  description: 'A confortable river view bamboo bungalows part of 5 exclusive cabins discretely nestled among trees. Built and equipped sustainably with the smallest footprint on its environment.',
+  price_per_night: 798,
+  number_of_guests: 2,
+  picture_url: 'https://a0.muscache.com/im/pictures/add21b43-7d35-4a8c-82f9-dee2c192b690.jpg?im_w=1440'
+)
+
+flat_3 = Flat.create(
+  name: 'Private Bungalow near Big Bend',
+  address: 'Terlingua, Texas, United States',
+  description: 'This bungalow boasts a minimalistic boho design, with small but unique decor touches that create a warm and inviting atmosphere.',
+  price_per_night: 504,
+  number_of_guests: 2,
+  picture_url: 'https://a0.muscache.com/im/pictures/1564b1af-125a-40c2-a633-a70f13745896.jpg?im_w=1440'
+)
